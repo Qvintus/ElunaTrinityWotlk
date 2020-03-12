67cc1e34752a05f7cf37a7c8bac8e0e473130c23
Nikolaj <qvintusdk@gmail.com>
2020-03-12 21:28:46 +0100

    .

---
+++ src/server/scripts/Custom/test_upgrade.cpp
@@ -0,0 +1,37 @@
+#include "ScriptMgr.h"
+#include "RBAC.h"
+#include "Item.h"
+
+class test_upgrade : public CommandScript
+{
+    public: 
+        test_upgrade() : CommandScript("test_upgrade") { }
+
+    std::vector<ChatCommand> GetCommands() const
+	{
+        std::vector<ChatCommand> commandtbl = {
+            {"titem1", rbac::RBAC_PERM_COMMAND_GM, true, &tItemEnchant1Handler, ""},
+            {"titem2", rbac::RBAC_PERM_COMMAND_GM, true, &tItemEnchant2Handler, ""}
+        }
+        return commandtbl;
+    }
+
+    void tItemEnchant1Handler(ChatHandler * handler, const char * args) {
+        Player* player = handler->GetSession()->GetPlayer();
+        Item* mainhand = player->GetItemByPos(255, EQUIPMENT_SLOT_MAINHAND);
+        uint32 val = GetUInt32Value(ITEM_FIELD_ENCHANTMENT_1_1);
+        TC_LOG_ERROR("entities.player.items", "Mainhand enchant 1_1: %d", val);
+    }
+
+    void tItemEnchant2Handler(ChatHandler * handler, const char * args) {
+        Player* player = handler->GetSession()->GetPlayer();
+        Item* mainhand = player->GetItemByPos(255, EQUIPMENT_SLOT_MAINHAND);
+        uint32 val = GetUInt32Value(ITEM_FIELD_ENCHANTMENT_1_3);
+        TC_LOG_ERROR("entities.player.items", "Mainhand enchant 1_3: %d", val);
+    }
+
+};
+
+void AddSC_test_upgrade {
+    new test_upgrade();
+}
