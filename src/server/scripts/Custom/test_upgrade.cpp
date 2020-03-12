ca4cd4e01c99c3a8c27fbeef01a85c7354b56945
Nikolaj <qvintusdk@gmail.com>
2020-03-12 22:27:09 +0100

    .

--- src/server/scripts/Custom/test_upgrade.cpp
+++ src/server/scripts/Custom/test_upgrade.cpp
@@ -21,14 +21,14 @@ class test_upgrade_class : public CommandScript
     }
 
     static void tItemEnchant1Handler(void* handler, ChatHandler* chatHandler, char const* argsStr) {
-        Player* player = handler->GetSession()->GetPlayer();
+        Player* player = chatHandler->GetSession()->GetPlayer();
         Item* mainhand = player->GetItemByPos(255, EQUIPMENT_SLOT_MAINHAND);
         uint32 val = mainhand->GetUInt32Value(ITEM_FIELD_ENCHANTMENT_1_1);
         TC_LOG_INFO("misc", "Mainhand enchant 1_1: %d", val);
     }
 
     static void tItemEnchant2Handler(void* handler, ChatHandler* chatHandler, char const* argsStr) {
-        Player* player = handler->GetSession()->GetPlayer();
+        Player* player = chatHandler->GetSession()->GetPlayer();
         Item* mainhand = player->GetItemByPos(255, EQUIPMENT_SLOT_MAINHAND);
         uint32 val = mainhand->GetUInt32Value(ITEM_FIELD_ENCHANTMENT_1_3);
         TC_LOG_INFO("misc", "Mainhand enchant 1_3: %d", val);
