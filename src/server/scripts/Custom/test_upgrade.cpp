7d6b5a23d4e0cc0bfb226c9fb105ffaf1b14ae4e
Nikolaj <qvintusdk@gmail.com>
2020-03-12 22:17:26 +0100

    .

--- src/server/scripts/Custom/test_upgrade.cpp
+++ src/server/scripts/Custom/test_upgrade.cpp
@@ -11,20 +11,20 @@ class test_upgrade_class : public CommandScript
     std::vector<ChatCommand> GetCommands() const
 	{
         std::vector<ChatCommand> commandtbl = {
-            {"titem1", rbac::RBAC_PERM_COMMAND_GM, true, test_upgrade_class::&tItemEnchant1Handler, ""},
-            {"titem2", rbac::RBAC_PERM_COMMAND_GM, true, test_upgrade_class::&tItemEnchant2Handler, ""}
+            {"titem1", rbac::RBAC_PERM_COMMAND_GM, true, &tItemEnchant1Handler, ""},
+            {"titem2", rbac::RBAC_PERM_COMMAND_GM, true, &tItemEnchant2Handler, ""}
         }
         return commandtbl;
     }
 
-    void tItemEnchant1Handler(ChatHandler * handler, const char * args) {
+    static void tItemEnchant1Handler(ChatHandler * handler, const char * args) {
         Player* player = handler->GetSession()->GetPlayer();
         Item* mainhand = player->GetItemByPos(255, EQUIPMENT_SLOT_MAINHAND);
         uint32 val = GetUInt32Value(ITEM_FIELD_ENCHANTMENT_1_1);
         TC_LOG_INFO("misc", "Mainhand enchant 1_1: %d", val);
     }
 
-    void tItemEnchant2Handler(ChatHandler * handler, const char * args) {
+    static void tItemEnchant2Handler(ChatHandler * handler, const char * args) {
         Player* player = handler->GetSession()->GetPlayer();
         Item* mainhand = player->GetItemByPos(255, EQUIPMENT_SLOT_MAINHAND);
         uint32 val = GetUInt32Value(ITEM_FIELD_ENCHANTMENT_1_3);
