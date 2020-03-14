8917c4cfcb5b8a46c7f9e7a057ef9eacdef6066f
Nikolaj <qvintusdk@gmail.com>
2020-03-14 12:23:08 +0100

    .

--- src/server/scripts/Custom/test_upgrade.cpp
+++ src/server/scripts/Custom/test_upgrade.cpp
@@ -23,8 +23,13 @@ class test_upgrade_class : public CommandScript
     static bool tItemEnchant1Handler(ChatHandler* chatHandler, char const* argsStr) {
         Player* player = chatHandler->GetSession()->GetPlayer();
         Item* mainhand = player->GetItemByPos(255, EQUIPMENT_SLOT_MAINHAND);
-        uint32 val = mainhand->GetUInt32Value(ITEM_FIELD_ENCHANTMENT_1_1);
-        TC_LOG_ERROR("misc", "Mainhand enchant 1_1: %d", val); // 0
+        for (uint8 i = 0; i < MAX_INSPECTED_ENCHANTMENT_SLOT; ++i) {
+            for(uint8 x = 0; x < MAX_ENCHANTMENT_OFFSET; ++x) {
+                uint32 val = mainhand->GetUInt32Value(ITEM_FIELD_ENCHANTMENT_1_1 + i*MAX_ENCHANTMENT_OFFSET + x);
+                TC_LOG_ERROR("misc", "Mainhand enchant 1_1: %d | Slot: %d | Type: %d", val, i, x);
+            }
+        }
+        
 
         return true;
     }
@@ -32,9 +37,12 @@ class test_upgrade_class : public CommandScript
     static bool tItemEnchant2Handler(ChatHandler* chatHandler, char const* argsStr) {
         Player* player = chatHandler->GetSession()->GetPlayer();
         Item* mainhand = player->GetItemByPos(255, EQUIPMENT_SLOT_MAINHAND);
-        uint32 val = mainhand->GetUInt32Value(ITEM_FIELD_ENCHANTMENT_1_3);
-        TC_LOG_ERROR("misc", "Mainhand enchant 1_3: %d", val); // 0
-
+        for (uint8 i = 0; i < MAX_INSPECTED_ENCHANTMENT_SLOT; ++i) {
+            for(uint8 x = 0; x < MAX_ENCHANTMENT_OFFSET; ++x) {
+                uint32 val = mainhand->GetUInt32Value(ITEM_FIELD_ENCHANTMENT_1_3 + i*MAX_ENCHANTMENT_OFFSET + x);
+                TC_LOG_ERROR("misc", "Mainhand enchant 1_3: %d | Slot: %d | Type: %d", val, i, x);
+            }
+        }
         return true;
     }
 
